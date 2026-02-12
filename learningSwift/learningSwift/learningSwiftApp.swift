e6567c0f14a18650546cf6dd7b10441bb035b9cb
James Choi <codejc04@gmail.com>
2026-02-12 11:26:56 -0500

    project init

---
+++ learningSwift/learningSwift/learningSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningSwiftApp.swift
+//  learningSwift
+//
+//  Created by James Choi on 2/12/26.
+//
+
+import SwiftUI
+
+@main
+struct learningSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
