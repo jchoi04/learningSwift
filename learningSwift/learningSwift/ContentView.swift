f4b77d8bd162ecb3861fb8e5be8ed5028fd9cb07
James Choi <codejc04@gmail.com>
2026-02-18 23:01:28 -0500

    lec2

--- learningSwift/learningSwift/ContentView.swift
+++ learningSwift/learningSwift/ContentView.swift
@@ -9,15 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            pegs(colors: [.red, .green, .green, .yellow])
+            pegs(colors: [.red, .blue, .green, .yellow])
+            pegs(colors: [.red, .yellow, .green, .blue])
         }
         .padding()
     }
+    
+    func pegs (colors: Array<Color>) -> some View{
+        HStack{
+            Circle() .foregroundColor(colors[0])
+            Circle() .foregroundColor(colors[1])
+            Circle() .foregroundColor(colors[2])
+            Circle() .foregroundColor(colors[3])
+        }
+    }
 }
+
 
 #Preview {
     ContentView()
